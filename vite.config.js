1cc46b8e944099dd5bc1b396c448621e3eedf853
anubhav0302 <anubhav@cyberpointmedia.com>
2025-11-17 20:46:15 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const base = import.meta.env.VITE_BASE_URL;
+// const base = import.meta.env.VITE_BASE_URL;
 export default defineConfig(({ mode }) => {
   // Load env variables based on the mode (development or production)
   const env = loadEnv(mode, process.cwd(), '');
-  console.log(env, 'live', base);
+  console.log(env, 'live');
   return {
-    base: base || '/',
+    base: env || '/',
     plugins: [react()],
   };
 });
